215819fa485b72da3b07ad2d6c9f7f64cefba610
Joël Ganesh <joel.ganesh@live.nl>
2024-03-03 03:50:57 +0100

    Issue fixed with Special00.

--- PsiElemIVS.cpp
+++ PsiElemIVS.cpp
@@ -10,6 +10,9 @@
 #define BRUTE_FORCE_DIAG 2
 #define BRUTE_FORCE_NONDIAG 3
 
+#define INFTY	LLONG_MAX
+#define MINFTY  LLONG_MIN
+
 namespace CompPsi
 {
 	// Returns the sign of a real number.
@@ -40,18 +43,26 @@ namespace CompPsi
 	// Algorithm by Helfgott & Thompson, 2023.
 	static int64_t FlCong(int64_t n, int64_t a, int64_t q)
 	{
+		if (n == MINFTY || n == INFTY)
+		{
+			return n;
+		}
 		return n - Mod(n - a, q);
 	}
 
 	// Returns the sum of g(n) over n in I with n = r mod q.
 	// G represents the 'q' partial sums of g, restricted to the congruence classes modulo q.
 	// Algorithm by Helfgott & Thompson, 2023.
-	static float_dec_100 SumInter(std::vector<float_dec_100> G, int64_t r, Interval I, int64_t b, int64_t q)
+	static int64_t SumInter(std::vector<int64_t> G, int64_t r, Interval I, int64_t b, int64_t q)
 	{
 		int64_t I0 = I.start, I1 = I.end;
 		if (I0 <= I1)
 		{
-			int64_t r0 = FlCong(I0 - 1, r, q);
+			if (I0 != MINFTY)
+			{
+				I0--;
+			}
+			int64_t r0 = FlCong(I0, r, q);
 			int64_t r1 = FlCong(std::min(I1, b - 1), r, q);
 			if (r0 > r1 || r1 < -b)
 			{
@@ -72,23 +83,22 @@ namespace CompPsi
 	// * sigma(r) = sum_{m in I_m : a0(m-m0) mod q > q - r} g(m).
 	// It is assumed that T can be cast to float_dec_100.
 	// Algorithm by Helfgott & Thompson, 2023.
-	template <typename T>
-	static std::tuple<std::vector<float_dec_100>, std::vector<float_dec_100>, std::vector<float_dec_100>>
-		SumTable(std::vector<T> g, int64_t q, int64_t b, int64_t m_index, int64_t a0)
+	static std::tuple<std::vector<int64_t>, std::vector<int64_t>, std::vector<int64_t>>
+		SumTable(std::vector<int> g, int64_t q, int64_t b, int64_t m_index, int64_t a0)
 	{
-		std::vector<float_dec_100> G(2 * b), rho(q), sigma(q + 1, 0);
+		std::vector<int64_t> G(2 * b), rho(q), sigma(q + 1, 0);
 		for (int64_t m = 0; m < q; m++)
 		{
-			G[m] = float_dec_100(g[m_index + m]);
+			G[m] = g[m_index + m];
 		}
 		for (int64_t m = q; m < b; m++)
 		{
-			G[m] = G[m - q] + float_dec_100(g[m_index + m]);
+			G[m] = G[m - q] + g[m_index + m];
 		}
 		int64_t r = Mod(a0 * (b - q), q);
 		int64_t a = Mod(a0, q);
 
-		for (int64_t m = b - q; m < b; m++)
+		for (int64_t m = 2 * b - q; m < 2 * b; m++)
 		{
 			rho[r] = G[m];
 			r += a;
@@ -108,22 +118,21 @@ namespace CompPsi
 	// Returns the sum of values of g(n) for n of the form b + kq, for integers k with sk < 0.
 	// It is assumed that T can be cast to float_dec_100.
 	// Algorithm by Helfgott & Thompson, 2023.
-	template <typename T>
-	static float_dec_100 RaySum(std::vector<T> g, int64_t q, int64_t b, int64_t m_index, int s)
+	static int64_t RaySum(std::vector<int> g, int64_t q, int64_t b, int64_t m_index, int s)
 	{
-		float_dec_100 S(0);
+		int64_t S(0);
 		if (s < 0)
 		{
 			for (int64_t m = b + q; m < 2 * b; m += q)
 			{
-				S += float_dec_100(g[m_index + m]);
+				S += g[m_index + m];
 			}
 		}
 		else if (s > 0)
 		{
 			for (int64_t m = q; m <= b; m += q)
 			{
-				S += float_dec_100(g[m_index + b - m]);
+				S += g[m_index + b - m];
 			}
 		}
 		return S;
@@ -133,27 +142,27 @@ namespace CompPsi
 	// by separation of variables.
 	// It is assumed that T1 and T2 can be cast to float_dec_100.
 	// Algorithm by Helfgott & Thompson, 2023.
-	template <typename T1, typename T2>
-	static float_dec_100 LinearSum(std::vector<T1> f, std::vector<T2> g,
+	static float_dec_100 LinearSum(std::vector<Log> f, std::vector<int> g,
 								   int64_t a, int64_t b, int64_t d_index, int64_t m_index,
 								   double alpha0, double alpha1, double alpha2)
 	{
-		float_dec_100 S_f0(0), S_f1(0), S_g0(0), S_g1(0);
+		float_dec_100 S_f0(0), S_f1(0);
+		int64_t S_g0(0), S_g1(0);
 		for (int64_t d = 0; d < 2 * a; d++)
 		{
-			S_f0 += float_dec_100(f[d_index + d]);
-			S_f1 += float_dec_100(f[d_index + d]) * std::floor(alpha0 + alpha1 * (d - a));
+			S_f0 += f[d_index + d].numerical();
+			S_f1 += f[d_index + d].numerical() * std::floor(alpha0 + alpha1 * (d - a));
 		}
 		for (int64_t m = 0; m < 2 * b; m++)
 		{
-			S_g0 += float_dec_100(g[m_index + m]);
-			S_g1 += float_dec_100(g[m_index + m]) * std::floor(alpha2 * (m - b));
+			S_g0 += g[m_index + m];
+			S_g1 += g[m_index + m] * std::floor(alpha2 * (m - b));
 		}
 		return S_f0 * S_g1 + S_f1 * S_g0;
 	}
 
 	// Computation of L - L1 for the special case where a0(m-m0) + r0 = -1 mod q, assuming q > 1.
-	static float_dec_100 Special1(std::vector<float_dec_100> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
+	static int64_t Special1(std::vector<int64_t> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
 								  double R0, int64_t r0, int64_t m0, int64_t d, int64_t b)
 	{
 		// Note that a0m + r0 = -1 mod q iff m = r mod q, with r = (-1 - r0) * a_inv.
@@ -164,26 +173,26 @@ namespace CompPsi
 
 		// J is actually the complement of the set we are trying to sum over.
 		// So we first sum over all values, then subtract the ones belonging to J.
-		return SumInter(G, r, Interval(LLONG_MIN, LLONG_MAX), b, q) - SumInter(G, r, J, b, q);
+		return SumInter(G, r, Interval(MINFTY, INFTY), b, q) - SumInter(G, r, J, b, q);
 	}
 
 	// Computing L - L1 for a0(m-m0) + r0 = 0 mod q, with q > 1.
 	// Algorithm by Helfgott & Thompson, 2023. Some comments are added for clarity.
-	static float_dec_100 Special0B(std::vector<float_dec_100> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
+	static int64_t Special0B(std::vector<int64_t> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
 								   double R0, int64_t r0, int64_t m0, int64_t d, int64_t b, double Q, int s_beta, int s_delta)
 	{
 		Interval I;
 		if (s_delta > 0)
 		{
-			I = Interval(LLONG_MIN, std::ceil(-Q) - 1);
+			I = Interval(MINFTY, std::ceil(-Q) - 1);
 		}
 		else if (s_delta < 0)
 		{
-			I = Interval(std::ceil(-Q) + 1, LLONG_MAX);
+			I = Interval(std::ceil(-Q) + 1, INFTY);
 		}
 		else if (s_beta < 0)
 		{
-			I = Interval(LLONG_MIN, LLONG_MAX);
+			I = Interval(MINFTY, INFTY);
 		}
 
 		int64_t gamma1 = d * (-std::floor(R0) * q - r0 + a * m0);
@@ -197,19 +206,19 @@ namespace CompPsi
 
 	// Computation of L - L1 for the special case where q = 1.
 	// Algorithm by Helfgott & Thompson, 2023. Some comments are added for clarity.
-	static float_dec_100 Special00(std::vector<float_dec_100> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
+	static int64_t Special00(std::vector<int64_t> G, int64_t N, int64_t q, int64_t a, int64_t a_inv,
 								   double R0, int64_t r0, int64_t m0, int64_t d, int64_t b, double Q, int s_delta)
 	{
-		Interval I, I_c;
+		Interval I, I_c(MINFTY, INFTY);
 		if (s_delta > 0)
 		{
-			I = Interval(LLONG_MIN, std::ceil(-Q) - 1);
-			I_c = Interval(std::ceil(-Q), LLONG_MAX);
+			I = Interval(MINFTY, std::ceil(-Q) - 1);
+			I_c = Interval(std::ceil(-Q), INFTY);
 		}
 		else if (s_delta < 0)
 		{
-			I = Interval(std::floor(-Q) + 1, LLONG_MAX);
-			I_c = Interval(LLONG_MIN, std::floor(-Q));
+			I = Interval(std::floor(-Q) + 1, INFTY);
+			I_c = Interval(MINFTY, std::floor(-Q));
 		}
 
 		std::vector<Interval> J(2);
@@ -222,22 +231,23 @@ namespace CompPsi
 			}
 			else
 			{
-				J[j] = Interval(LLONG_MIN, N / (d * ((int64_t)std::floor(R0) + r0 + j)) - m0);
+				J[j] = Interval(N / (d * (int64_t)std::floor(R0) + r0 + j) - m0 + 1, INFTY);
+				//J[j] = Interval(MINFTY, N / (d * ((int64_t)std::floor(R0) + r0 + j)) - m0);
 			}
 		}
 
 		// The intervals J[0] and J[1] are actually the complements of the sets J'[0] and J'[1] we want;
 		// we try to sum over the set (I \cap J'[0]) \sqcup (I_c \cap J'[1]).
 		// Note that (I \cap J'[0]) \sqcup (I_c \cap J'[1]) = R - (I \cap J[0]) - (I_c \cap J[1]).
-		J[0].Intersect(I);
-		J[1].Intersect(I_c);
-		return SumInter(G, 0, Interval(LLONG_MIN, LLONG_MAX), b, q)
+		J[0].Intersect(I); //J[0].Shift(b);
+		J[1].Intersect(I_c); //J[1].Shift(b);
+		return SumInter(G, 0, Interval(MINFTY, INFTY), b, q)
 			- SumInter(G, 0, J[0], b, q) - SumInter(G, 0, J[1], b, q);
 	}
 
 	// Computation of L1 - L2 for the special case where a0(m-m0) + r0 = 0 mod q.
 	// Algorithm by Helfgott & Thompson, 2023. Some comments are added for clarity.
-	static float_dec_100 Special0A(std::vector<float_dec_100> G, int64_t q, int64_t a, int64_t a_inv,
+	static int64_t Special0A(std::vector<int64_t> G, int64_t q, int64_t a, int64_t a_inv,
 								   int64_t r0, int64_t b, double Q, int s_beta, int s_delta)
 	{
 		// Note that am + r0 = 0 mod q iff m = r mod q with r = -r0 * a_inv. 
@@ -249,15 +259,15 @@ namespace CompPsi
 			// The interval I should be taken s.t. m in I iff beta + delta m >= 0.
 			if (s_delta > 0)
 			{
-				I = Interval(std::ceil(-Q), LLONG_MAX);
+				I = Interval(std::ceil(-Q), INFTY);
 			}
 			else if (s_delta < 0)
 			{
-				I = Interval(LLONG_MIN, std::floor(-Q));
+				I = Interval(MINFTY, std::floor(-Q));
 			}
 			else if (s_beta >= 0) // s_delta = 0.
 			{
-				I = Interval(LLONG_MIN, LLONG_MAX);
+				I = Interval(MINFTY, INFTY);
 			}
 		}
 		else
@@ -268,13 +278,13 @@ namespace CompPsi
 			{
 				if (s_delta < 0)
 				{
-					return SumInter(G, r, Interval(LLONG_MIN, std::floor(-Q)), b, q)
-						+ SumInter(G, r, Interval(1, LLONG_MAX), b, q);
+					return SumInter(G, r, Interval(MINFTY, std::floor(-Q)), b, q)
+						+ SumInter(G, r, Interval(1, INFTY), b, q);
 				}
 				else if (s_delta > 0)
 				{
-					return SumInter(G, r, Interval(LLONG_MIN, -1), b, q)
-						+ SumInter(G, r, Interval(std::ceil(-Q), LLONG_MAX), b, q);
+					return SumInter(G, r, Interval(MINFTY, -1), b, q)
+						+ SumInter(G, r, Interval(std::ceil(-Q), INFTY), b, q);
 				}
 			}
 			else if (s_beta > 0)
@@ -296,8 +306,7 @@ namespace CompPsi
 	// Algorithm by Helfgott & Thompson, 2023.
 	// It is assumed that a and b are such that the error in
 	// the linear approximation of N/xy is less than 1/2b.
-	template <typename T1, typename T2>
-	static float_dec_100 SumByLinAppr(std::vector<T1> f, std::vector<T2> g, int64_t N, int64_t d0, int64_t m0,
+	static float_dec_100 SumByLinAppr(std::vector<Log> f, std::vector<int> g, int64_t N, int64_t d0, int64_t m0,
 									  int64_t a, int64_t b, int64_t d_index, int64_t m_index)
 	{
 		double alpha0 = (double)N / (d0 * m0), alpha1 = -(double)N / (d0 * d0 * m0), alpha2 = -(double)N / (d0 * m0 * m0);
@@ -310,17 +319,17 @@ namespace CompPsi
 		int sgn_delta = std::get<3>(tuple);
 		double delta = alpha2 - (double)a0 / q;
 
-		float_dec_100 Z = RaySum(g, q, b, m_index, sgn_delta);
+		int64_t raySum = RaySum(g, q, b, m_index, sgn_delta);
 
-		std::tuple<std::vector<float_dec_100>, std::vector<float_dec_100>, std::vector<float_dec_100>> 
+		std::tuple<std::vector<int64_t>, std::vector<int64_t>, std::vector<int64_t>>
 			tuple2 = SumTable(g, q, b, m_index, a0);
-		std::vector<float_dec_100> G = std::get<0>(tuple2);
-		std::vector<float_dec_100> rho = std::get<1>(tuple2);
-		std::vector<float_dec_100> sigma = std::get<2>(tuple2);
+		std::vector<int64_t> G = std::get<0>(tuple2);
+		std::vector<int64_t> rho = std::get<1>(tuple2);
+		std::vector<int64_t> sigma = std::get<2>(tuple2);
 
 		for (int64_t d = 0; d < 2 * a; d++)
 		{
-			if (float_dec_100(f[d_index + d]) != 0)
+			if (f[d_index + d].n > 1) // In words means that f[d_index + d] is non-zero.
 			{
 				double R0 = alpha0 + alpha1 * (d - a);
 				double R0_frac = R0 - std::floor(R0);
@@ -334,14 +343,18 @@ namespace CompPsi
 				{
 					Q = beta / delta;
 				}
-				float_dec_100 T = sigma[r0] + Special0A(G, q, a0, a0_inv, r0, b, Q, sgn_beta, sgn_delta);
 
+				// T represents the sum of mu(m) * (L(d, m) - L2(d, m)).
+				int64_t T(0);
+
+				// Consideration of difference L(d, m) - L1(d, m) for m : a0(m - m0) + r0 = 0, -1 mod q.
+				// (For other values of m, the difference is 0.)
 				if (q > 1)
 				{
-					// Account for case a0(d-d0) + r0 = -1 mod q.
+					// Account for case a0(m - m0) + r0 = -1 mod q.
 					T += Special1(G, N, q, a0, a0_inv, R0, r0, m0, d_, b);
 
-					// Account for case a0(d-d0) + r0 = 0 mod q.
+					// Account for case a0(m - m0) + r0 = 0 mod q.
 					T += Special0B(G, N, q, a0, a0_inv, R0, r0, m0, d_, b, Q, sgn_beta, sgn_delta);
 				}
 				else // Case q = 1.
@@ -349,14 +362,20 @@ namespace CompPsi
 					T += Special00(G, N, q, a0, a0_inv, R0, r0, m0, d_, b, Q, sgn_delta);
 				}
 
+				// Consideration of difference L1(d, m) - L2(d, m) for m : a0(m - m0) + r0 != 0 mod q.
+				T += sigma[r0];
 				if (0 < r0 && r0 < q)
 				{
-					T += Z;
+					T += raySum;
 				}
+
+				// Consideration of difference L1(d, m) - L2(d, m) for m : a0(m - m0) + r0 = 0 mod q.
+				T += Special0A(G, q, a0, a0_inv, r0, b, Q, sgn_beta, sgn_delta);
 
 				S += float_dec_100(f[d_index + d]) * T;
 			}
 		}
+		std::cout << d0 - a << " " << d0 + a - 1 << " " << m0 - b << " " << m0 + b - 1 << ": " << S << std::endl;
 		return S;
 	}
 
@@ -381,9 +400,8 @@ namespace CompPsi
 
 	// Computes the sum of f(d)g(m)floor(N/dm) over the rectangle [d0, d1) x [m0, m1) by
 	// linear approximation of the term floor(N/dm) on small rectangles of fixed size a * b.
-	template <typename T1, typename T2>
 	static float_dec_100 DoubleSum(int64_t d0, int64_t d1, int64_t m0, int64_t m1, int64_t a, int64_t b,
-								   std::vector<T1> f, std::vector<T2> g, int64_t N)
+								   std::vector<Log> f, std::vector<int> g, int64_t N)
 	{
 		float_dec_100 S(0);
 		for (int64_t mm = m0; mm < m1; mm += 2 * a)
@@ -419,16 +437,22 @@ namespace CompPsi
 				switch (mode)
 				{
 					case LINEAR_APPR_DIAG:
-						S += DoubleSum(d0, d1, m0, m1, a, b, mu_d, Lambda_m, N);
+						std::cout << "Lin-appr Diag: " << m0 << " " << m1 << " " << d0 << " " << d1 << std::endl;
+						std::cout << DoubleSum(m0, m1, d0, d1, b, a, Lambda_m, mu_d, N) << std::endl;
+						S += DoubleSum(m0, m1, d0, d1, b, a, Lambda_m, mu_d, N);
 						break;
 					case LINEAR_APPR_NONDIAG:
-						S += DoubleSum(d0, d1, m0, m1, a, b, mu_d, Lambda_m, N);
+						std::cout << "Lin-appr Non-diag: " << m0 << " " << m1 << " " << d0 << " " << d1 << std::endl;
+						std::cout << DoubleSum(m0, m1, d0, d1, b, a, Lambda_m, mu_d, N) + DoubleSum(d0, d1, m0, m1, a, b, Lambda_d, mu_m, N) << std::endl;
+						S += DoubleSum(m0, m1, d0, d1, b, a, Lambda_m, mu_d, N);
 						S += DoubleSum(d0, d1, m0, m1, a, b, Lambda_d, mu_m, N);
 						break;
 					case BRUTE_FORCE_DIAG:
+						std::cout << BruteDoubleSum(m0, m1, d0, d1, Lambda_m, mu_d, N) << std::endl;
 						S += BruteDoubleSum(m0, m1, d0, d1, Lambda_m, mu_d, N);
 						break;
 					case BRUTE_FORCE_NONDIAG:
+						std::cout << BruteDoubleSum(m0, m1, d0, d1, Lambda_m, mu_d, N) + BruteDoubleSum(d0, d1, m0, m1, Lambda_d, mu_m, N) << std::endl;
 						S += BruteDoubleSum(m0, m1, d0, d1, Lambda_m, mu_d, N);
 						S += BruteDoubleSum(d0, d1, m0, m1, Lambda_d, mu_m, N);
 						break;
